4f3188e78f9b2a37cedc5968c2a9fee28214f6fe
xyt <xyt1831322993@outlook.com>
2019-08-23 22:22:14 +0800

    777

---
+++ EduMS.MVCSite/Areas/Admin/Controllers/TeacherController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EduMS.MVCSite.Models.Admin;
+using EduMS.BLL;
+using System.Threading.Tasks;
+
+namespace EduMS.MVCSite.Areas.Admin.Controllers
+{
+    public class TeacherController : Controller
+    {
+        // GET: Admin/Teacher
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+
+        [HttpGet]       //请求指定页面的信息，并返回实体主体
+        public ActionResult CreateTeacher()
+        {
+            return View();
+        }
+
+        [HttpPost]      //向指定资源提交数据进行处理请求
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> CreateTeacher(TeacherViewModel model)
+        {
+            if (!ModelState.IsValid) return View(model);
+            IBLL.ITeacherManager Manager = new ITeacherManager();
+            await Manager.AddTeacher(model.TeaId, model.TeaName,model.TeaGender,model.TeaTele,model.TeaIDNumber,model.Teapwd,model.TeaDepartment);
+            return Content("添加成功");
+        }
+    }
+}
---
+++ EduMS.MVCSite/Models/Admin/TeacherViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EduMS.MVCSite.Models.Admin
+{
+    public class TeacherViewModel
+    {
+        [Required, StringLength(10)]    ///required表明该项用户必须填写
+        [Display(Name = "教师ID")]
+        public string TeaId { get; set; }
+
+        [Required, StringLength(10)]
+        [Display(Name = "教师姓名")]
+        public string TeaName { get; set; }
+
+        [Required]
+        [Display(Name = "性别")]
+        public string TeaGender { get; set; }
+
+        [Required, StringLength(11)]
+        [Display(Name = "电话号码")]
+        public string TeaTele { get; set; }
+
+        [StringLength(18)]
+        [Display(Name = "身份证号")]
+        public string TeaIDNumber { get; set; }
+
+        [Required, StringLength(12)]
+        [Display(Name = "密码")]
+        public string Teapwd { get; set; }
+
+        [Required, StringLength(12)]
+        [Display(Name = "院系")]
+        public string TeaDepartment { get; set; }
+    }
+}
